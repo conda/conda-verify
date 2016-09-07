ab0ca6ce80d27719082adbdc935c4d1067eaee3e
Ilan Schnell <ilanschnell@gmail.com>
2016-09-07 11:25:31 -0500

    add exception for conda-build also

--- anaconda_verify/package.py
+++ anaconda_verify/package.py
@@ -211,7 +211,7 @@ class CondaPackageCheck(object):
                     print("WARNING: %s next to: %s" % (ext, p))
 
     def no_pyc_in_stdlib(self):
-        if self.name in {'python', 'scons'}:
+        if self.name in {'python', 'scons', 'conda-build'}:
             return
         for p in self.paths:
             if p.endswith('.pyc') and not 'site-packages' in p:

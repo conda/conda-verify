9c4e1cac0c422fe829776cee134967af0c723c22
sophia <scastellarin95@gmail.com>
2016-11-03 11:49:48 -0500

    Refactor verification to run scripts dynamically and expose simple api
    This is achieved by:
    - Adding seperate scripts for validating recipes and packages
    - Using verify class to run verification scripts
    - Deleting unused modules (package and recipe) in favour of new architecture
    - renaming anaconda_verify to conda_verify

---
+++ conda_verify/exceptions.py
@@ -0,0 +1,6 @@
+class RecipeError(Exception):
+    pass
+
+
+class PackageError(Exception):
+    pass
---
+++ conda_verify/package/test_windows_arch.py
@@ -0,0 +1,7 @@
+from conda_verify.conda_package_check import CondaPackageCheck
+
+
+def verify(path_to_package=None, verbose=True, **kwargs):
+    package_check = CondaPackageCheck(path_to_package, verbose)
+    package_check.check_windows_arch()
+    package_check.t.close()

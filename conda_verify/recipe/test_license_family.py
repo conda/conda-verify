d3699ab0a4991cf4eca2e800a9f36e7b9a901463
sophia <scastellarin95@gmail.com>
2016-11-09 13:53:37 -0600

    Break up tests into their own files

--- conda_verify/recipe/test_license_family.py
+++ conda_verify/recipe/test_license_family.py
@@ -3,5 +3,5 @@ from conda_verify.conda_recipe_check import CondaRecipeCheck
 
 def verify(rendered_meta=None, recipe_dir=None, **kwargs):
     recipe_check = CondaRecipeCheck(rendered_meta, recipe_dir)
-    recipe_check.validate_files()
-    recipe_check.check_dir_content()
+    pedantic = kwargs.get("pedantic") if "pedantic" in kwargs.keys() else True
+    recipe_check.check_license_family(pedantic)

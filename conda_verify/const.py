18ade2a095ce0c50994a50ad59951d416577e023
mandeep <mbhutani@continuum.io>
2017-06-27 12:14:17 -0500

    Add extra field with recipe-maintainers as sole value

--- conda_verify/const.py
+++ conda_verify/const.py
@@ -28,6 +28,7 @@ FIELDS = {
     'test': {'requires', 'commands', 'files', 'imports'},
     'about': {'license', 'license_url', 'license_family', 'license_file',
               'summary', 'description', 'home', 'doc_url', 'dev_url'},
+    'extra': {'recipe-maintainers'},
 }
 
 MAGIC_HEADERS = {

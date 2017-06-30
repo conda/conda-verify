66f885011261ee9bbef2b7f15433ba43292f6532
mandeep <mbhutani@continuum.io>
2017-06-30 15:01:35 -0500

    Add missing pedantic argument to test functions

--- conda_verify/package/test_files.py
+++ conda_verify/package/test_files.py
@@ -2,14 +2,14 @@ from conda_verify.conda_package_check import CondaPackageCheck
 
 
 def verify_files(path_to_package=None, verbose=True, **kwargs):
+    pedantic = kwargs.get("pedantic") if "pedantic" in kwargs.keys() else True
     package_check = CondaPackageCheck(path_to_package, verbose)
     package_check.info_files()
     package_check.no_hardlinks()
-    package_check.not_allowed_files()
+    package_check.not_allowed_files(pedantic=pedantic)
     package_check.index_json()
     package_check.no_bat_and_exe()
     package_check.list_packages()
-    pedantic = kwargs.get("pedantic") if "pedantic" in kwargs.keys() else True
     package_check.has_prefix(pedantic=pedantic)
 
     package_check.t.close()
--- conda_verify/package/test_setup_files.py
+++ conda_verify/package/test_setup_files.py
@@ -2,9 +2,9 @@ from conda_verify.conda_package_check import CondaPackageCheck
 
 
 def verify_setup_files(path_to_package=None, verbose=True, **kwargs):
+    pedantic = kwargs.get("pedantic") if "pedantic" in kwargs.keys() else True
     package_check = CondaPackageCheck(path_to_package, verbose)
     package_check.no_setuptools()
-    pedantic = kwargs.get("pedantic") if "pedantic" in kwargs.keys() else True
     package_check.no_easy_install_script(pedantic)
-    package_check.no_pth()
+    package_check.no_pth(pedantic)
     package_check.t.close()

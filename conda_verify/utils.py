14e8b877c7315a97736a8baa5a067941734ac7dc
mandeep <mandeep@users.noreply.github.com>
2017-07-26 23:05:37 -0500

    Replace custom memoization decorator with functools.lru_cache
    Change lru_cache maxsize from 128 to 32

--- conda_verify/utils.py
+++ conda_verify/utils.py
@@ -1,4 +1,3 @@
-import collections
 import os
 import re
 import sys
@@ -6,30 +5,13 @@ import yaml
 
 from conda_verify.constants import MAGIC_HEADERS, DLL_TYPES
 
-
-class memoized(object):
-    """Decorator. Caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned
-    (not reevaluated).
-    """
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args):
-        if not isinstance(args, collections.Hashable):
-            # uncacheable. a list, for instance.
-            # better to not cache than blow up.
-            return self.func(*args)
-        if args in self.cache:
-            return self.cache[args]
-        else:
-            value = self.func(*args)
-            self.cache[args] = value
-            return value
+try:
+    from functools import lru_cache
+except ImportError:
+    from backports.functools_lru_cache import lru_cache
 
 
-@memoized
+@lru_cache(maxsize=32)
 def yamlize(data):
     res = yaml.load(data)
     # ensure the result is a dict
--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 # (c) 2017 Continuum Analytics, Inc. / http://continuum.io
 # All Rights Reserved
 from setuptools import setup
+import sys
 
 from conda_verify import __version__
+
+if sys.version_info.major == 2:
+    requirements = ['backports-functools-lru-cache']
+else:
+    requirements = []
 
 
 setup(
@@ -15,6 +21,7 @@ setup(
     description="A tool for validating conda recipes and conda packages",
     long_description=open('README.md').read(),
     packages=['conda_verify'],
+    install_requires=requirements,
     entry_points='''
         [console_scripts]
         conda-verify=conda_verify.cli:cli

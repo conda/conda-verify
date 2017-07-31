85aa8173e17c2818a9944ad6b4907829879b6cda
mandeep <mbhutani@continuum.io>
2017-07-31 12:50:30 -0500

    Add check for CondaRecipeCheck

--- conda_verify/verify.py
+++ conda_verify/verify.py
@@ -2,13 +2,14 @@
 import sys
 
 from conda_verify.checks import CondaPackageCheck, CondaRecipeCheck
-from conda_verify.errors import PackageError
+from conda_verify.errors import PackageError, RecipeError
+from conda_verify.utilities import ensure_list
 
 
 class Verify(object):
 
     @staticmethod
-    def verify_package(path_to_package=None):
+    def verify_package(path_to_package=None, checks_to_ignore=None, exit_on_error=False):
         package_check = CondaPackageCheck(path_to_package)
 
         checks_to_display = [getattr(package_check, method)() for method
@@ -18,19 +19,26 @@ class Verify(object):
         return_code = 0
 
         for check in sorted(checks_to_display):
-            print(check, file=sys.stderr)
-            return_code = 1
+            if check.code not in ensure_list(checks_to_ignore):
+                print(check, file=sys.stderr)
+                return_code = 1
 
         if return_code > 0:
             sys.exit(return_code)
 
     @staticmethod
-    def verify_recipe(rendered_meta=None, recipe_dir=None):
+    def verify_recipe(rendered_meta=None, recipe_dir=None, checks_to_ignore=None, exit_on_error=False):
         recipe_check = CondaRecipeCheck(rendered_meta, recipe_dir)
-        recipe_check.check_fields()
-        recipe_check.check_source()
-        recipe_check.check_requirements()
-        recipe_check.check_for_valid_files()
-        recipe_check.check_about()
-        recipe_check.check_license_family()
-        recipe_check.check_dir_content()
+
+        checks_to_display = [getattr(recipe_check, method)() for method
+                             in dir(recipe_check) if method.startswith('check') and
+                             getattr(recipe_check, method)() is not None]
+
+        return_code = 0
+
+        for check in sorted(checks_to_display):
+            print(check, file=sys.stderr)
+            return_code = 1
+
+        if return_code > 0:
+            sys.exit(return_code)

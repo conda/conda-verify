6179779a2fef3fd5a9b3d6cc520617882ae452bc
mandeep <mbhutani@continuum.io>
2017-07-31 15:10:28 -0500

    Refactor package tests due to exception change

--- tests/functional_tests/test_invalid_packages.py
+++ tests/functional_tests/test_invalid_packages.py
@@ -38,370 +38,356 @@ def test_invalid_package_extension(package_dir, verifier):
             'Found package with invalid extension ".zip"' in str(excinfo))
 
 
-def test_duplicate_members(package_dir, verifier):
+def test_duplicate_members(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.1-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "duplicate members" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1116 Found duplicate members inside tar archive' in error
 
 
-def test_index_unicode(package_dir, verifier):
+def test_index_unicode(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.2-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "non-ASCII in: info/index.json" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1115 Found non-ascii characters inside info/index.json' in error
 
 
-def test_info_in_files_file(package_dir, verifier):
+def test_info_in_files_file(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.3-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "Did not expect 'info/index.json' in info/files" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1119 Found filenames in info/files that start with "info"' in error
 
 
-def test_duplicates_in_files_file(package_dir, verifier):
+def test_duplicates_in_files_file(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.4-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "info/files: duplicates" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1120 Found duplicate filenames in info/files' in error
 
 
 def test_not_in_files_file(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.5-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
     output, error = capfd.readouterr()
 
-    assert ("PackageError: "
-            "info/files" in str(excinfo))
-
-    assert "'lib/testfile.txt' not in info/files" in str(output)
+    assert 'C1122 Found filename in tar archive missing from info/files: lib/testfile.txt' in error
 
 
 def test_not_in_tarball(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.6-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
     output, error = capfd.readouterr()
 
-    assert ("PackageError: "
-            "info/files" in str(excinfo))
-
-    assert "'testfile.txt' not in tarball" in str(output)
+    assert 'C1121 Found filename in info/files missing from tar archive: testfile.txt' in error
 
 
-def test_not_allowed_files(package_dir, verifier):
+def test_not_allowed_files(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.7-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "directory or filename not allowed: "
-            "info/testfile~" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1124 Found unallowed file in tar archive: info/testfile~' in error
 
 
-def test_file_not_allowed(package_dir, verifier):
+def test_file_not_allowed(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.8-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "file not allowed: info/link.json" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1125 Found info/link.json however package is not a noarch package' in error
 
 
-def test_invalid_package_name(package_dir, verifier):
+def test_invalid_package_name(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.9-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "info/index.json for name: " in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1102 Found package name in info/index.json "test-file" does not match filename "testfile"' in error
 
 
-def test_invalid_build_number(package_dir, verifier):
+def test_invalid_build_number(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.10-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "info/index.json: invalid build_number: 1" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1108 Build number in info/index.json must be an integer' in error
 
 
-def test_duplicates_in_bin(package_dir, verifier):
+def test_duplicates_in_bin(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.11-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "Both .bat and .exe files:" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1126 Found both .bat and .exe files in executable directory' in error
 
 
 def test_win_package_warning(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.12-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
     output, error = capfd.readouterr()
 
-    assert ("PackageError: "
-            "info/has_prefix: target 'bin/testfile' "
-            "not in package" in str(excinfo))
-
-    assert 'WARNING: info/has_prefix' in str(output)
+    assert 'C1128 Found filename "bin/testfile" in info/has_prefix not included in archive' in error
 
 
 def test_win_package_binary_warning(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.13-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "binary placeholder not allowed on Windows" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1131 Binary placeholder found in info/has_prefix not allowed in Windows package' in error
 
 
-def test_package_placeholder_warning(package_dir, verifier):
+def test_package_placeholder_warning(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.14-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("info/has_prefix: binary placeholder not 255 bytes, "
-            "but: 329" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1132 Binary placeholder' in error
+    assert 'found in info/has_prefix does not have a length of 255 bytes' in error
 
 
-def test_invalid_prefix_mode(package_dir, verifier):
+def test_invalid_prefix_mode(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.15-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "info/has_prefix: invalid mode" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1129 Found invalid mode "wrong_mode" in info/has_prefix' in error
 
 
-def test_unicode_prefix(package_dir, verifier):
+def test_unicode_prefix(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.16-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "non-ASCII in: info/has_prefix" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1127 Found non-ascii characters in info/has_prefix' in error
 
 
 def test_invalid_script_name(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.17-py36_0.tar.bz2')
 
-    verifier.verify_package(path_to_package=package)
+    with pytest.raises(SystemExit):
+        verifier.verify_package(path_to_package=package)
 
     output, error = capfd.readouterr()
 
-    assert "WARNING: bin/test-pre-unlink.bat" in str(output)
+    assert 'Found pre/post link file "bin/test-pre-unlink.bat" in archive' in error
 
 
-def test_invalid_setuptools(package_dir, verifier):
+def test_invalid_setuptools(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.18-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "file 'bin/easy_install.pth' not allowed" in str(excinfo))
+    output, error = capfd.readouterr()
+    
+    assert 'C1135 Found easy_install script "bin/easy_install.pth" in archive' in error
+    assert 'C1136 Found namespace file "bin/easy_install.pth" in archive' in error
 
 
-def test_invalid_eggfile(package_dir, verifier):
+def test_invalid_eggfile(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.19-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "file 'bin/test.egg' not allowed" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1134 Found egg file "bin/test.egg" in archive' in error
 
 
-def test_invalid_script_header(package_dir, verifier):
+def test_invalid_script_header(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.20-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "easy install script found: bin/test-script.py" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'Csomething easy_install_script found' in error
 
 
 def test_invalid_namespace_file(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.21-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "found namespace .pth file 'bin/test-nspkg.pth'" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1136 Found namespace file "bin/test-nspkg.pth" in archive' in error
 
 
 def test_invalid_pyo_file(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.22-py36_0.tar.bz2')
 
-    verifier.verify_package(path_to_package=package)
+    with pytest.raises(SystemExit):
+        verifier.verify_package(path_to_package=package)
 
     output, error = capfd.readouterr()
 
-    assert "WARNING: .pyo file: bin/test.pyo" in str(output)
+    assert 'C1137 Found pyo file "bin/test.pyo" in archive' in error
 
 
 def test_invalid_pyc_and_so_files(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.23-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
-
-    assert ("PackageError: "
-            ".pyc found in stdlib: bin/test.pyc" in str(excinfo))
 
     output, error = capfd.readouterr()
 
-    assert "WARNING: .pyc next to: bin/test.so" in str(output)
+    assert 'C1137 Found pyo file "bin/test.pyo" in archive' in error
+    assert 'C1138 Found pyc file "bin/test.pyc" in invalid directory' in error
 
 
-def test_invalid_pickle_file(package_dir, verifier):
+def test_invalid_pickle_file(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.24-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "found lib2to3 .pickle: lib/lib2to3/test.pickle" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1139 Found lib2to3 .pickle file "lib/lib2to3/test.pickle"' in error
 
 
 def test_missing_pyc_file(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.25-py27_0.tar.bz2')
 
-    verifier.verify_package(path_to_package=package)
+    with pytest.raises(SystemExit):
+        verifier.verify_package(path_to_package=package)
 
     output, error = capfd.readouterr()
 
-    assert ("WARNING: pyc missing for:" in str(output))
+    assert  'C1140 Found python file "lib/site-packages/python2.7/test.py" without a corresponding pyc file' in error
 
 
-def test_invalid_windows_architecture(package_dir, verifier):
+def test_invalid_windows_architecture(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.26-py27_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
-        verifier.verify_package(path_to_package=package)
-
-    assert ("PackageError: "
-            "Unrecognized Windows architecture: x84" in str(excinfo))
-
-
-def test_invalid_windows_dll(package_dir, verifier):
-    package = os.path.join(package_dir, 'testfile-0.0.27-py27_0.tar.bz2')
-
-    with pytest.raises(PackageError) as excinfo:
-        verifier.verify_package(path_to_package=package)
-
-    assert ("PackageError: "
-            "File bin/testfile.dll has object type None, "
-            "but info/index.json arch is x86_64" in str(excinfo))
-
-
-def test_invalid_package_in_name(package_dir, verifier, capfd):
-    package = os.path.join(package_dir, 'testfile-0.0.28-py27_0.tar.bz2')
-
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
     output, error = capfd.readouterr()
 
-    assert ("PackageError: "
-            "found numpy" in str(excinfo))
-
-    assert 'numpy' in str(output)
+    assert 'C1143 Found unrecognized Windows architecture "x84"' in error
 
 
-def test_invalid_package_in_bin(package_dir, verifier, capfd):
-    package = os.path.join(package_dir, 'testfile-0.0.29-py27_0.tar.bz2')
+def test_invalid_windows_dll(package_dir, verifier, capfd):
+    package = os.path.join(package_dir, 'testfile-0.0.27-py27_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "found setuptools" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1144 Found file "bin/testfile.dll" with object type "None" but with arch "x86_64"' in error
 
 
 def test_invalid_easy_install_file(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.31-py27_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "easy-install.pth file not allowed" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1136 Found namespace file "bin/easy-install.pth" in archive' in error
 
 
-def test_empty_tar(package_dir, verifier):
-    package = os.path.join(package_dir, 'testfile-0.0.38-py36_0.tar')
-
-    with pytest.raises(KeyError) as excinfo:
-        verifier.verify_package(path_to_package=package)
-
-    assert "not found" in str(excinfo)
-
-
-def test_non_ascii_path(package_dir, verifier):
+def test_non_ascii_path(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.39-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert "PackageError: non-ASCII path" in str(excinfo)
+    output, error = capfd.readouterr()
+
+    assert 'C1117 Found archive member names containing non-ascii characters' in error
 
 
-def test_ascii_in_files_file(package_dir, verifier):
+def test_ascii_in_files_file(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.40-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert "PackageError: non-ASCII in: info/files" in str(excinfo)
+    output, error = capfd.readouterr()
+
+    assert 'Found filenames in info/files containing non-ascii characters' in error
 
 
-def test_missing_depends_key(package_dir, verifier):
+def test_missing_depends_key(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.41-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: "
-            "info/index.json: key 'depends' missing" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'C1112 Missing "depends" field in info/index.json' in error
 
 
-def test_invalid_license_family(package_dir, verifier):
+def test_invalid_license_family(package_dir, verifier, capfd):
     package = os.path.join(package_dir, 'testfile-0.0.42-py36_0.tar.bz2')
 
-    with pytest.raises(PackageError) as excinfo:
+    with pytest.raises(SystemExit):
         verifier.verify_package(path_to_package=package)
 
-    assert ("PackageError: wrong license family: FAKELICENSE" in str(excinfo))
+    output, error = capfd.readouterr()
+
+    assert 'Found invalid license "FAKELICENSE" in info/index.json' in error

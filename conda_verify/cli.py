82d537c243d7774590ea15b9b3407ef9e93cc501
mandeep <mandeep@users.noreply.github.com>
2017-07-31 19:52:33 -0500

    Add ignore and exit options to CLI

--- conda_verify/cli.py
+++ conda_verify/cli.py
@@ -8,17 +8,24 @@ from conda_verify.utilities import render_metadata, iter_cfgs
 
 @click.command()
 @click.argument('path', type=click.Path(exists=True))
+@click.option('--ignore', nargs=1, type=str)
+@click.option('--exit', is_flag=True)
 @click.version_option(prog_name='conda-verify')
-def cli(path):
+def cli(path, ignore, exit):
     """"""
     verifier = Verify()
+    if ignore:
+        ignore = ignore.split(',')
+
     meta_file = os.path.join(path, 'meta.yaml')
     if os.path.isfile(meta_file):
         print('Verifying {}...' .format(meta_file))
         for cfg in iter_cfgs():
             meta = render_metadata(path, cfg)
-            verifier.verify_recipe(rendered_meta=meta, recipe_dir=path)
+            verifier.verify_recipe(rendered_meta=meta, recipe_dir=path,
+                                   checks_to_ignore=ignore, exit_on_error=exit)
 
     elif path.endswith(('.tar.bz2', '.tar')):
         print('Verifying {}...' .format(path))
-        verifier.verify_package(path_to_package=path)
+        verifier.verify_package(path_to_package=path, checks_to_ignore=ignore,
+                                exit_on_error=exit)

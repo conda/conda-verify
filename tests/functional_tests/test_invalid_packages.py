bb4998be90aea4cbaeae17b37436991af27e5761
mandeep <mandeep@users.noreply.github.com>
2017-10-14 19:20:15 -0500

    Add test for check_inded_dependencies_specs

--- tests/functional_tests/test_invalid_packages.py
+++ tests/functional_tests/test_invalid_packages.py
@@ -446,3 +446,14 @@ def test_invalid_package_placeholder(package_dir, verifier, capfd):
     output, error = capfd.readouterr()
 
     assert 'C1129 Found filename "/opt/anaconda1anaconda2anaconda3 text testfile testfile" in info/has_prefix not included in archive' in error
+
+
+def test_invalid_dependency_specs(package_dir, verifier, capfd):
+    package = os.path.join(package_dir, 'testfile-0.0.48-py27_0.tar.bz2')
+
+    with pytest.raises(SystemExit):
+        verifier.verify_package(path_to_package=package)
+
+    output, error = capfd.readouterr()
+
+    assert 'C1114 Found invalid dependency "python 3.6@**&*&(&@!" in info/index.json' in error

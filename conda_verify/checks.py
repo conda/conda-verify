959e18e25c15b84ea2b6d9b83dbd4edf3a0e323a
mandeep <mandeep@users.noreply.github.com>
2017-10-14 19:36:14 -0500

    Add test for package version check and move check if statement so that it hits

--- conda_verify/checks.py
+++ conda_verify/checks.py
@@ -74,12 +74,12 @@ class CondaPackageCheck(object):
         package_version = str(self.info.get('version'))
         if not package_version:
             return Error(self.path, 'C1104', 'Missing package version in info/index.json')
+        if package_version.startswith(('_', '.')) or package_version.endswith(('_', '.')):
+            return Error(self.path, 'C1107', "Package version in info/index.json cannot start or end with '_' or '.'")
         if not self.version_pat.match(package_version) or get_bad_seq(package_version):
             return Error(self.path, 'C1105', 'Found invalid version number in info/index.json')
         if package_version != self.version:
             return Error(self.path, 'C1106', u'Found package version in info/index.json "{}" does not match filename version "{}"' .format(package_version, self.version))
-        if package_version.startswith(('_', '.')) or package_version.endswith(('_', '.')):
-            return Error(self.path, 'C1107', "Package version in info/index.json cannot start or end with '_' or '.'")
 
     def check_build_number(self):
         """Check the build number located in info/index.json."""
--- tests/functional_tests/test_invalid_packages.py
+++ tests/functional_tests/test_invalid_packages.py
@@ -490,3 +490,14 @@ def test_invalid_build_number_negative(package_dir, verifier, capfd):
     output, error = capfd.readouterr()
 
     assert 'C1109 Build number in info/index.json cannot be a negative integer' in error
+
+
+def test_invalid_version_suffix(package_dir, verifier, capfd):
+    package = os.path.join(package_dir, 'testfile-0.0.52-py36_0.tar.bz2')
+
+    with pytest.raises(SystemExit):
+        verifier.verify_package(path_to_package=package)
+
+    output, error = capfd.readouterr()
+
+    assert "C1107 Package version in info/index.json cannot start or end with '_' or '.'" in error

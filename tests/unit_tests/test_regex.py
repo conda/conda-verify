215323fd952cee961920a0f08cfb3604bca8ff08
mandeep <mandeep@users.noreply.github.com>
2017-12-03 16:34:03 -0600

    Refactor tests to use utilities.fullmatch

--- tests/unit_tests/test_regex.py
+++ tests/unit_tests/test_regex.py
@@ -1,5 +1,4 @@
 import os
-import re
 
 import pytest
 
@@ -25,6 +24,7 @@ def test_ver_spec_pat(package_dir, recipe_dir):
     package_ver_spec_pat = package_check.ver_spec_pat
     recipe_ver_spec_pat = recipe_check.ver_spec_pat
 
+    extra_spec = '>===3.5'
     ge_version = '>=1.2'
     eq_version = '==1.2.2'
     pin_version = '>=2,<3'
@@ -32,16 +32,17 @@ def test_ver_spec_pat(package_dir, recipe_dir):
     or_version = '1.0|1.2*'
     regex_version = '3.6*'
 
-    assert re.match(package_ver_spec_pat, ge_version)
-    assert re.match(package_ver_spec_pat, eq_version)
-    assert re.match(package_ver_spec_pat, pin_version)
-    assert re.match(package_ver_spec_pat, pin_version_long)
-    assert re.match(package_ver_spec_pat, or_version)
-    assert re.match(package_ver_spec_pat, regex_version)
+    assert not utilities.fullmatch(package_ver_spec_pat, extra_spec)
+    assert utilities.fullmatch(package_ver_spec_pat, ge_version)
+    assert utilities.fullmatch(package_ver_spec_pat, eq_version)
+    assert utilities.fullmatch(package_ver_spec_pat, pin_version)
+    assert utilities.fullmatch(package_ver_spec_pat, pin_version_long)
+    assert utilities.fullmatch(package_ver_spec_pat, or_version)
+    assert utilities.fullmatch(package_ver_spec_pat, regex_version)
 
-    assert re.match(recipe_ver_spec_pat, ge_version)
-    assert re.match(recipe_ver_spec_pat, eq_version)
-    assert re.match(recipe_ver_spec_pat, pin_version)
-    assert re.match(recipe_ver_spec_pat, pin_version_long)
-    assert re.match(recipe_ver_spec_pat, or_version)
-    assert re.match(recipe_ver_spec_pat, regex_version)
+    assert utilities.fullmatch(recipe_ver_spec_pat, ge_version)
+    assert utilities.fullmatch(recipe_ver_spec_pat, eq_version)
+    assert utilities.fullmatch(recipe_ver_spec_pat, pin_version)
+    assert utilities.fullmatch(recipe_ver_spec_pat, pin_version_long)
+    assert utilities.fullmatch(recipe_ver_spec_pat, or_version)
+    assert utilities.fullmatch(recipe_ver_spec_pat, regex_version)

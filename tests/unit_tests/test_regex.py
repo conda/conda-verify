dbb9c6c0e554977b495c9bba10ad56df60825b5f
mandeep <mandeep@users.noreply.github.com>
2017-12-03 16:51:11 -0600

    Fix issue with version specs with trailing periods

--- tests/unit_tests/test_regex.py
+++ tests/unit_tests/test_regex.py
@@ -29,8 +29,9 @@ def test_ver_spec_pat(package_dir, recipe_dir):
     eq_version = '==1.2.2'
     pin_version = '>=2,<3'
     pin_version_long = '<=2.0.0*,<3.0.0*'
-    or_version = '1.0|1.2*'
+    or_version = '1.0|1.2.*'
     regex_version = '3.6*'
+    python_version = '3.6.*'
 
     assert not utilities.fullmatch(package_ver_spec_pat, extra_spec)
     assert utilities.fullmatch(package_ver_spec_pat, ge_version)
@@ -39,10 +40,13 @@ def test_ver_spec_pat(package_dir, recipe_dir):
     assert utilities.fullmatch(package_ver_spec_pat, pin_version_long)
     assert utilities.fullmatch(package_ver_spec_pat, or_version)
     assert utilities.fullmatch(package_ver_spec_pat, regex_version)
+    assert utilities.fullmatch(package_ver_spec_pat, python_version)
 
+    assert not utilities.fullmatch(recipe_ver_spec_pat, extra_spec)
     assert utilities.fullmatch(recipe_ver_spec_pat, ge_version)
     assert utilities.fullmatch(recipe_ver_spec_pat, eq_version)
     assert utilities.fullmatch(recipe_ver_spec_pat, pin_version)
     assert utilities.fullmatch(recipe_ver_spec_pat, pin_version_long)
     assert utilities.fullmatch(recipe_ver_spec_pat, or_version)
     assert utilities.fullmatch(recipe_ver_spec_pat, regex_version)
+    assert utilities.fullmatch(recipe_ver_spec_pat, python_version)

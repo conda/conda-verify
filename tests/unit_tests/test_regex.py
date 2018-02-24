e1757813fd08b562ea36dc4c5b6ce06795405d54
mandeep <mandeep@users.noreply.github.com>
2018-02-24 12:27:29 -0600

    Add pre-release versioning to version check regex pattern

--- tests/unit_tests/test_regex.py
+++ tests/unit_tests/test_regex.py
@@ -29,6 +29,8 @@ def test_ver_spec_pat(package_dir, recipe_dir):
     eq_version = '==1.2.2'
     pin_version = '>=2,<3'
     pin_version_long = '<=2.0.0*,<3.0.0*'
+    prerelease_version = '2.0rc1'
+    pin_prerelease_version = '>=1.9.3,<2.0.0a0'
     or_version = '1.0|1.2.*'
     regex_version = '3.6*'
     python_version = '3.6.*'
@@ -38,6 +40,8 @@ def test_ver_spec_pat(package_dir, recipe_dir):
     assert utilities.fullmatch(package_ver_spec_pat, eq_version)
     assert utilities.fullmatch(package_ver_spec_pat, pin_version)
     assert utilities.fullmatch(package_ver_spec_pat, pin_version_long)
+    assert utilities.fullmatch(package_ver_spec_pat, prerelease_version)
+    assert utilities.fullmatch(package_ver_spec_pat, pin_prerelease_version)
     assert utilities.fullmatch(package_ver_spec_pat, or_version)
     assert utilities.fullmatch(package_ver_spec_pat, regex_version)
     assert utilities.fullmatch(package_ver_spec_pat, python_version)
@@ -47,6 +51,8 @@ def test_ver_spec_pat(package_dir, recipe_dir):
     assert utilities.fullmatch(recipe_ver_spec_pat, eq_version)
     assert utilities.fullmatch(recipe_ver_spec_pat, pin_version)
     assert utilities.fullmatch(recipe_ver_spec_pat, pin_version_long)
+    assert utilities.fullmatch(recipe_ver_spec_pat, prerelease_version)
+    assert utilities.fullmatch(recipe_ver_spec_pat, pin_prerelease_version)
     assert utilities.fullmatch(recipe_ver_spec_pat, or_version)
     assert utilities.fullmatch(recipe_ver_spec_pat, regex_version)
     assert utilities.fullmatch(recipe_ver_spec_pat, python_version)
